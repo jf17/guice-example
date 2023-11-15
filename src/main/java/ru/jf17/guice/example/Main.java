41f17073ebfe6f7521360a201c7facc8f958843f
alex <javafirst17@gmail.com>
2023-11-16 01:18:13 +0300

    Refactoring

--- src/main/java/ru/jf17/guice/example/Main.java
+++ src/main/java/ru/jf17/guice/example/Main.java
@@ -7,8 +7,8 @@ import ru.jf17.guice.example.controller.MyController;
 
 public class Main {
     public static void main(String[] args) {
-        Injector injector = Guice.createInjector(new DatabaseInjector());
-        MyController app = injector.getInstance(MyController.class);
+        final Injector injector = Guice.createInjector(new DatabaseInjector());
+        final MyController app = injector.getInstance(MyController.class);
         System.out.println(app.getDatabaseData());
 
     }
